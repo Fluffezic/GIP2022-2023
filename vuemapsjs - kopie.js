2daa9db0524346c19c853ffa9de332d9bef5c369
FluffEzic <42347002+Fluffezic@users.noreply.github.com>
2023-04-17 13:23:31 +0200

    Working guessing system
    added guessing system
    added randomized location
    added distance calculator

--- vuemapsjs - kopie.js
+++ vuemapsjs - kopie.js
@@ -1,12 +1,88 @@
-let map;
+var mLocation;
+var resultset;
+var distanceP;
+var mLocations = [];
+var Melevation;
+var Search ;
 var latc;
 var lngc;
 var country;
+var Validlocation = false;
+var randomlat;
+var randomlng;
 function initMap() {
 	geocoder = new google.maps.Geocoder();
-	var randomlat = (Math.random()*180) - 90
-	var randomlng = (Math.random()*360) - 180
-  map = new google.maps.Map(document.getElementById("map"), {
+	elevator = new google.maps.ElevationService();
+ Search = setInterval(RandomCheck, 500);
+
+}
+
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+	messages:"Guess where you are",
+	city:"",
+  },
+  methods: {
+    Guess: function () {
+	country = this.city;
+     geocoder.geocode( {'address' : country}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+ resultset = results[0].geometry.location;
+	latc = resultset.lat;
+	lngc = resultset.lng;
+	 distanceP = google.maps.geometry.spherical.computeDistanceBetween(mLocation, resultset);
+
+	
+	
+ app.messages = "your points: " + Math.round((19134300 - distanceP)/100)/1000 };
+});
+  
+    }
+}
+ })
+function RandomCheck() {
+	console.log("test1");
+		 randomlat = (Math.random()*160 + 10) - 90;
+	 randomlng = (Math.random()*360) - 180;
+mLocation=  new google.maps.LatLng(randomlat, randomlng);
+mLocations.push(mLocation) ;
+geocoder.geocode( {location : mLocation}, function(status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+		}
+			elevator.getElevationForLocations({locations: mLocations,}, function(results, status) {
+				Melevation = results;
+
+}			
+
+			);
+	console.log("test3");
+			console.log(Melevation[0].elevation);
+		if (Melevation[0].elevation > 90){
+;
+					Validlocation = true;
+		  clearInterval(Search);
+		GenerateMap();
+		window.initMap = initMap;
+		}
+else {
+	mLocations = [];
+	console.log("test5");
+	Validlocation = false ;
+
+	}
+		
+		
+		
+});
+
+
+}
+
+function GenerateMap() {
+	  map = new google.maps.Map(document.getElementById("map"), {
 	
     center: { lat: randomlat, lng: randomlng },
     zoom: 6,
@@ -32,26 +108,6 @@ map.set('styles',customStyled);
 latc = map.getCenter().lat();
 lngc = map.getCenter().lng();
 console.log(latc + " " + lngc);
+	
+	
 }
-
-window.initMap = initMap;
-
-var app = new Vue({
-  el: '#app',
-  data: {
-	messages:"placeholder",
-	city:"",
-  },
-  methods: {
-    Guess: function () {
-	country = this.city;
-     geocoder.geocode( {'address' : country}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-	var resultset = results[0].geometry.location;
- app.messages = resultset  }
-});
-
-    }
-}
- })
-
